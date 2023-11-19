bfdc21889d5c13594b17bfd51261d3169f801efa
RafliFadhilah <151422845+RafliFadhilah@users.noreply.github.com>
2023-11-19 14:43:02 +0700

    Update body.c

--- C/body.c
+++ C/body.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 #include <conio.h>
-#include "header.h"
+#include <stdlib.h>
+#include "sipaling_header.h"
 
-void MenuAwal(){
-	char key;
-	
+void MenuAwal(){		
 	printf("%*s\n\n",18,"MENU");
 	printf("%*s\n",21,"(1)Start");
 	printf("%*s\n",20,"(2)Exit");
-	
 	key = getch();
+}
 	
+void MenuGame(){
 	if (key=='1'){
 		system("cls");
 		printf("%*s\n\n",27,"MODE PERMAINAN");
 		printf("%*s\n",31,"(1)Player 1 vs Player 2");
 		printf("%*s\n",31,"(2)Player 1 vs Komputer");
 		
+		key = getch();
+		
+		if (key=='1'){
+			system("cls");
+			MasukkanPemain();
+			void Papan();
+
+		} else if (key=='2'){
+			system("cls");
+			printf("%*s\n\n",1,"hihih");
+		}
+		
 	} else if (key=='2'){
 		system("cls");
 		printf("%*s\n",18,"DADAH");	
 	}
+}
 
-	
-	
+void MasukkanPemain(){
+	printf("%*s\n\n",28 ,"MASUKKAN NAMA PEMAIN");
+	printf("%*s",20,"Player 1 -> ");
+	fgets(NamaPemain1,50, stdin);
+	printf("%*s",20,"Player 2 -> ");
+	fgets(NamaPemain2,50, stdin);
+}
+
+void Papan (){
 	
 }
